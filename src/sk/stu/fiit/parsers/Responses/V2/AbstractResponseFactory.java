a4e5737b0412b17267b286671fd69ee5d491c42d
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-30 00:33:57 +0200

    Added: Javadoc for Response and AbstractResponseFactory

--- src/sk/stu/fiit/parsers/Responses/V2/AbstractResponseFactory.java
+++ src/sk/stu/fiit/parsers/Responses/V2/AbstractResponseFactory.java
@@ -9,11 +9,24 @@ import sk.stu.fiit.Exceptions.APIValidationException;
 import sk.stu.fiit.Exceptions.AuthTokenExpiredException;
 
 /**
+ * AbstractResponseFactory interface is used to define common method that every
+ * factory must implement
  *
  * @author Adam Bublavý
  */
 public interface AbstractResponseFactory<T> {
 
+    /**
+     * Parses received response from the server and returns data from this
+     * response mapped in the object
+     *
+     * @param response Received response from the server
+     * @return Returns data mapped in the object
+     * @throws AuthTokenExpiredException is thrown, when JWT token which is used
+     * for authentication has expired
+     * @throws APIValidationException is thrown, when the request has invalid
+     * data
+     */
     T parse(CloseableHttpResponse response) throws AuthTokenExpiredException,
             APIValidationException;
 
