2155fc33b6f7c60ee9dedb30ef73deeb817ccff0
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-29 13:52:28 +0200

    Added: Javadoc four DeleteTourOfferRequest

--- src/sk/stu/fiit/parsers/Requests/dto/DeleteTourOfferRequest.java
+++ src/sk/stu/fiit/parsers/Requests/dto/DeleteTourOfferRequest.java
@@ -7,6 +7,7 @@ package sk.stu.fiit.parsers.Requests.dto;
 import sk.stu.fiit.parsers.Requests.IRequestVisitor;
 
 /**
+ * DeleteTourOfferRequest request is used to delete tour offer
  *
  * @author Adam Bublavý
  */
@@ -14,10 +15,21 @@ public class DeleteTourOfferRequest extends Request {
 
     private String id;
 
+    /**
+     * Creates new DeleteTourOfferRequest
+     *
+     * @param id ID of tour offer which will be deleted
+     */
     public DeleteTourOfferRequest(String id) {
         this.id = id;
     }
-    
+
+    /**
+     * {@inheritDoc}
+     *
+     * {@link IRequestVisitor#constructDeleteTourOfferRequest(sk.stu.fiit.parsers.Requests.dto.DeleteTourOfferRequest)
+     * }
+     */
     @Override
     public void accept(IRequestVisitor visitor) {
         visitor.constructDeleteTourOfferRequest(this);
@@ -26,5 +38,5 @@ public class DeleteTourOfferRequest extends Request {
     public String getId() {
         return id;
     }
-    
+
 }
