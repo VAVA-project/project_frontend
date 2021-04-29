8e94cfe8b9fcab2cddb59f2de61cfae99d754c1a
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-30 00:53:03 +0200

    Added: Javadoc for TicketToCartResponse

--- src/sk/stu/fiit/parsers/Responses/V2/AddTicketToCartResponses/TicketToCartResponse.java
+++ src/sk/stu/fiit/parsers/Responses/V2/AddTicketToCartResponses/TicketToCartResponse.java
@@ -7,13 +7,22 @@ package sk.stu.fiit.parsers.Responses.V2.AddTicketToCartResponses;
 import sk.stu.fiit.parsers.Responses.V2.Response;
 
 /**
+ * TicketToCartResponse response is used to hold data which are extracted from
+ * adding or removing ticket from the user's cart.
  *
  * @author adamf
+ * 
+ * @see AddTicketToCartRequest
+ * @see DeleteTicketFromCartRequest
  */
 public class TicketToCartResponse extends Response {
-    
+
     private boolean isTicketAddedToCart;
 
+    /**
+     * Creates new TicketToCartResponse
+     * @param isTicketAddedToCart
+     */
     public TicketToCartResponse(boolean isTicketAddedToCart) {
         this.isTicketAddedToCart = isTicketAddedToCart;
     }
@@ -21,5 +30,5 @@ public class TicketToCartResponse extends Response {
     public boolean isIsTicketAddedToCart() {
         return isTicketAddedToCart;
     }
-    
+
 }
