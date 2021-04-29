2b1579900580d75737f80e16a9fb35136f205e88
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-30 01:08:13 +0200

    Added: Javadoc for DeleteCartResponse

--- src/sk/stu/fiit/parsers/Responses/V2/DeleteCartResponses/DeleteCartResponse.java
+++ src/sk/stu/fiit/parsers/Responses/V2/DeleteCartResponses/DeleteCartResponse.java
@@ -7,13 +7,21 @@ package sk.stu.fiit.parsers.Responses.V2.DeleteCartResponses;
 import sk.stu.fiit.parsers.Responses.V2.Response;
 
 /**
+ * DeleteCartResponse response is used to hold data which are extracted from
+ * deleting user's cart response
  *
  * @author Adam Bublavý
+ * 
+ * @see DeleteCartRequest
  */
 public class DeleteCartResponse extends Response {
-    
+
     private boolean success;
 
+    /**
+     * Creates new DeleteCartResponse
+     * @param success true if user's cart was deleted successfully, false otherwise
+     */
     public DeleteCartResponse(boolean success) {
         this.success = success;
     }
@@ -21,5 +29,5 @@ public class DeleteCartResponse extends Response {
     public boolean isSuccess() {
         return success;
     }
-    
+
 }
