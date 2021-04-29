cfeb8bd5271963e6b5de2e35a4d07e6e5be657a0
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-29 14:24:50 +0200

    Added: Javadoc for UserBookingsRequest

--- src/sk/stu/fiit/parsers/Requests/dto/UserBookingsRequest.java
+++ src/sk/stu/fiit/parsers/Requests/dto/UserBookingsRequest.java
@@ -7,14 +7,21 @@ package sk.stu.fiit.parsers.Requests.dto;
 import sk.stu.fiit.parsers.Requests.IRequestVisitor;
 
 /**
+ * UserBookingsRequest request is used to fetch all user's bookings
  *
  * @author Adam Bublavý
  */
 public class UserBookingsRequest extends Request {
 
+    /**
+     * {@inheritDoc}
+     *
+     * {@link IRequestVisitor#constructUserBookingsRequest(sk.stu.fiit.parsers.Requests.dto.UserBookingsRequest)
+     * }
+     */
     @Override
     public void accept(IRequestVisitor visitor) {
         visitor.constructUserBookingsRequest(this);
     }
-    
+
 }
