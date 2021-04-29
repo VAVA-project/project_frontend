a655fd45ebc61acf1043b0a330090f7ff5b23222
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-30 00:50:52 +0200

    Added: Javadoc for XMLProcessor

--- src/sk/stu/fiit/parsers/Responses/V2/XMLProcessor.java
+++ src/sk/stu/fiit/parsers/Responses/V2/XMLProcessor.java
@@ -29,11 +29,20 @@ import sk.stu.fiit.Exceptions.AuthTokenExpiredException;
 import sk.stu.fiit.parsers.Responses.V2.RegisterResponses.RegisterResponseProcessor;
 
 /**
+ * XMLProcessor represents abstract response parser which parses XML data
  *
  * @author Adam Bublavý
  */
 public abstract class XMLProcessor implements ResponseProcessor {
 
+    /**
+     * {@inheritDoc}
+     *
+     * Creates document from received response and parses it into Response
+     *
+     * @see Document
+     * @see Response
+     */
     @Override
     public Response processResponse(CloseableHttpResponse response) throws
             AuthTokenExpiredException, APIValidationException {
@@ -68,10 +77,38 @@ public abstract class XMLProcessor implements ResponseProcessor {
         return null;
     }
 
+    /**
+     * Gets list of all possible validation errors which can be in received
+     * request. List defines tag names whose value will be extracted.
+     *
+     * @return Returns list of all possible validation errors
+     */
     public abstract List<String> getPossibleValidationErrors();
 
+    /**
+     * This method is used to parse received response. It is called only if http
+     * status code was SC_OK.
+     *
+     * @param document Created document from received response
+     * @return Returns extracted data
+     *
+     * @see Document
+     * @see Response
+     */
     public abstract Response parseOK(Document document);
 
+    /**
+     * Parses validation errors which was sent in response. This method is
+     * called when http status code of the response was SC_BAD_REQUEST.
+     *
+     * @param document Created document from received response
+     * @param possibleValidationErrors List of possible validation errors which
+     * can occur in response
+     * @return Returns list of parsed validation errors
+     *
+     * @see APIValidationError
+     * {@link XMLProcessor#getPossibleValidationErrors() }
+     */
     public List<APIValidationError> parseValidationErrors(Document document,
             List<String> possibleValidationErrors) {
         XPath xPath = XPathFactory.newInstance().newXPath();
@@ -90,24 +127,34 @@ public abstract class XMLProcessor implements ResponseProcessor {
                         getTagName())) {
                     continue;
                 }
-                
+
                 if (errorElement.getChildNodes().getLength() > 1) {
                     validationErrors.addAll(this.parseNodeList(errorElement.
                             getChildNodes(),
                             possibleValidationErrors));
                 } else {
-                    validationErrors.add(new APIValidationError(errorElement.getTagName(),
+                    validationErrors.add(new APIValidationError(errorElement.
+                            getTagName(),
                             errorElement.getTextContent()));
                 }
             }
         } catch (XPathExpressionException ex) {
-            System.out.println("TODO");
             System.out.println("XPathExpressionException: " + ex.getMessage());
         }
 
         return validationErrors;
     }
 
+    /**
+     * Parses list of nodes and extracts errors
+     *
+     * @param nodeList List of nodes
+     * @param possibleValidationErrors List of possible validation errors which
+     * can occur in response
+     * @return Returns list of parsed validation errors
+     *
+     * @see APIValidationError
+     */
     private List<APIValidationError> parseNodeList(NodeList nodeList,
             List<String> possibleValidationErrors) {
         if (nodeList == null) {
