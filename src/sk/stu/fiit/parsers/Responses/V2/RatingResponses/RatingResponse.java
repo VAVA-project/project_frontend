c80ec81498383c23cd9a7f68a67371f8d2e13e34
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-30 01:31:35 +0200

    Added: Javadoc for RatingResponse

--- src/sk/stu/fiit/parsers/Responses/V2/RatingResponses/RatingResponse.java
+++ src/sk/stu/fiit/parsers/Responses/V2/RatingResponses/RatingResponse.java
@@ -7,22 +7,33 @@ package sk.stu.fiit.parsers.Responses.V2.RatingResponses;
 import sk.stu.fiit.parsers.Responses.V2.Response;
 
 /**
+ * RatingResponse response is used to hold data which are extracted from rating
+ * tour offer response
  *
  * @author Adam Bublavý
+ *
+ * @see RatingRequest
  */
 public class RatingResponse extends Response {
-    
+
     private String tourOfferId;
     private Integer rating;
     private Double averageRating;
 
+    /**
+     * Creates new RatingResponse
+     *
+     * @param tourOfferId ID of rated tour offer
+     * @param rating User's rating
+     * @param averageRating Actual average rating of rated tour offer
+     */
     public RatingResponse(String tourOfferId, Integer rating,
             Double averageRating) {
         this.tourOfferId = tourOfferId;
         this.rating = rating;
         this.averageRating = averageRating;
     }
-    
+
     public String getTourOfferId() {
         return tourOfferId;
     }
@@ -34,5 +45,5 @@ public class RatingResponse extends Response {
     public Double getAverageRating() {
         return averageRating;
     }
-    
+
 }
