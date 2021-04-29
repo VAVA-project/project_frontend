7de8d9993999d4a41931b0bd82736532d36b00f6
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-30 00:58:01 +0200

    Added: Javadoc for TicketToCartResponseFactory

--- src/sk/stu/fiit/parsers/Responses/V2/AddTicketToCartResponses/TicketToCartResponseFactory.java
+++ src/sk/stu/fiit/parsers/Responses/V2/AddTicketToCartResponses/TicketToCartResponseFactory.java
@@ -12,27 +12,39 @@ import sk.stu.fiit.parsers.Responses.V2.AbstractResponseFactory;
 import sk.stu.fiit.parsers.Responses.V2.Response;
 
 /**
+ * TicketToCartResponseFactory is used to check type of the received response
+ * and call particular response processor
  *
  * @author adamf
  */
-public class TicketToCartResponseFactory implements AbstractResponseFactory<Response> {
-    
+public class TicketToCartResponseFactory implements
+        AbstractResponseFactory<Response> {
+
     private TicketToCartResponseFactory() {
     }
-    
+
+    /**
+     * @return new instance of TicketToCartResponseFactory
+     */
     public static TicketToCartResponseFactory getInstance() {
         return new TicketToCartResponseFactory();
     }
-    
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see TicketToCartResponseProcessor
+     */
     @Override
-    public Response parse(CloseableHttpResponse response) throws AuthTokenExpiredException, APIValidationException {
+    public Response parse(CloseableHttpResponse response) throws
+            AuthTokenExpiredException, APIValidationException {
         Header header = response.getFirstHeader("Content-Type");
-        
-        if(header.getValue().equals("application/xml;charset=UTF-8")) {
+
+        if (header.getValue().equals("application/xml;charset=UTF-8")) {
             return new TicketToCartResponseProcessor().processResponse(response);
         }
-        
+
         return null;
     }
-    
+
 }
