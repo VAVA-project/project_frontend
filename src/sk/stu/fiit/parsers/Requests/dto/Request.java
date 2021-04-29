a7ce6202a9fd770202ff58dcccb645db9f33f3df
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-29 13:38:58 +0200

    Added: Javadoc for Request, AddTicketToCartRequest

--- src/sk/stu/fiit/parsers/Requests/dto/Request.java
+++ src/sk/stu/fiit/parsers/Requests/dto/Request.java
@@ -8,19 +8,30 @@ import org.apache.http.client.methods.HttpRequestBase;
 import sk.stu.fiit.parsers.Requests.IRequest;
 
 /**
+ * Request represents abstract request that will be passed to API
  *
  * @author Adam Bublavý
  */
 public abstract class Request implements IRequest {
-    
+
     private HttpRequestBase request;
 
+    /**
+     * Returns Http request which was created after accepting visitor
+     *
+     * @return Returns Http request for API
+     */
     public HttpRequestBase getRequest() {
         return request;
     }
 
+    /**
+     * Sets Http request. This method is used by visitors
+     *
+     * @param request Http request that will be set
+     */
     public void setRequest(HttpRequestBase request) {
         this.request = request;
     }
-    
+
 }
