2f6aff94ca2db1ba380f5f4091bf09e9b2149b80
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-29 13:43:21 +0200

    Added: Javadoc for CreateTourDateRequest

--- src/sk/stu/fiit/parsers/Requests/dto/CreateTourDateRequest.java
+++ src/sk/stu/fiit/parsers/Requests/dto/CreateTourDateRequest.java
@@ -8,24 +8,45 @@ import java.time.LocalDateTime;
 import sk.stu.fiit.parsers.Requests.IRequestVisitor;
 
 /**
+ * CreateTourDateRequest request is used to create new tour date for specific
+ * tour offer
  *
  * @author Adam Bublavý
+ *
+ * @see Tour
+ * @see TourDate
  */
 public class CreateTourDateRequest extends Request {
 
     private String tourOfferId;
-    
+
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private Integer numberOfTickets;
 
-    public CreateTourDateRequest(String tourOfferId, LocalDateTime startDate, LocalDateTime endDate, Integer numberOfTickets) {
+    /**
+     * Creates new CreateTourDateRequest
+     *
+     * @param tourOfferId ID of tour offer for which this tour date will be
+     * created
+     * @param startDate Start date
+     * @param endDate End date
+     * @param numberOfTickets Number of tickets for this tour date
+     */
+    public CreateTourDateRequest(String tourOfferId, LocalDateTime startDate,
+            LocalDateTime endDate, Integer numberOfTickets) {
         this.tourOfferId = tourOfferId;
         this.startDate = startDate;
         this.endDate = endDate;
         this.numberOfTickets = numberOfTickets;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * {@link IRequestVisitor#constructCreateTourDateRequest(sk.stu.fiit.parsers.Requests.dto.CreateTourDateRequest)
+     * }
+     */
     @Override
     public void accept(IRequestVisitor visitor) {
         visitor.constructCreateTourDateRequest(this);
@@ -46,5 +67,5 @@ public class CreateTourDateRequest extends Request {
     public Integer getNumberOfTickets() {
         return numberOfTickets;
     }
-    
+
 }
