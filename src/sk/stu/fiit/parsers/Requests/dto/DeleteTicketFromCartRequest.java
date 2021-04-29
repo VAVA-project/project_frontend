e85a2d0253af714f6f8b6512e07ebb27dc93d9a8
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-29 13:49:41 +0200

    Added: Javadoc for DeleteTicketFromCartRequest

--- src/sk/stu/fiit/parsers/Requests/dto/DeleteTicketFromCartRequest.java
+++ src/sk/stu/fiit/parsers/Requests/dto/DeleteTicketFromCartRequest.java
@@ -4,20 +4,36 @@
  */
 package sk.stu.fiit.parsers.Requests.dto;
 
+import sk.stu.fiit.Main.TourTicket;
 import sk.stu.fiit.parsers.Requests.IRequestVisitor;
 
 /**
+ * DeleteTicketFromCartRequest request is used to delete the ticket from the
+ * user's cart
  *
  * @author adamf
  */
 public class DeleteTicketFromCartRequest extends Request {
-    
+
     private String id;
 
+    /**
+     * Creates new DeleteTicketFromCartRequest
+     *
+     * @param id ID of the ticket
+     *
+     * @see TourTicket
+     */
     public DeleteTicketFromCartRequest(String id) {
         this.id = id;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * {@link IRequestVisitor#constructDeleteTicketFromCartRequest(sk.stu.fiit.parsers.Requests.dto.DeleteTicketFromCartRequest)
+     * }
+     */
     @Override
     public void accept(IRequestVisitor visitor) {
         visitor.constructDeleteTicketFromCartRequest(this);
@@ -26,5 +42,5 @@ public class DeleteTicketFromCartRequest extends Request {
     public String getId() {
         return id;
     }
-    
+
 }
