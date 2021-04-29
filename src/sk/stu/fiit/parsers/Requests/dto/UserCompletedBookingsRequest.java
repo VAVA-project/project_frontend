d66d41c1d267883dcbf01c4d35b1afafa6212d23
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-29 14:25:43 +0200

    Added: Javadoc for UserCompletedBookingsRequest

--- src/sk/stu/fiit/parsers/Requests/dto/UserCompletedBookingsRequest.java
+++ src/sk/stu/fiit/parsers/Requests/dto/UserCompletedBookingsRequest.java
@@ -7,14 +7,22 @@ package sk.stu.fiit.parsers.Requests.dto;
 import sk.stu.fiit.parsers.Requests.IRequestVisitor;
 
 /**
+ * UserCompletedBookingsRequest request is used to fetch all user's completed
+ * tours
  *
  * @author Adam Bublavý
  */
 public class UserCompletedBookingsRequest extends Request {
 
+    /**
+     * {@inheritDoc}
+     *
+     * {@link IRequestVisitor#constructUserCompletedBookingsRequest(sk.stu.fiit.parsers.Requests.dto.UserCompletedBookingsRequest)
+     * }
+     */
     @Override
     public void accept(IRequestVisitor visitor) {
         visitor.constructUserCompletedBookingsRequest(this);
     }
-    
+
 }
