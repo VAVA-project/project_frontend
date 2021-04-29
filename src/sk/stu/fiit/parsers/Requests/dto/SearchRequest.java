d2d6a724d7592672174fbdd54e4259a746fdb2d3
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-29 14:20:06 +0200

    Added: Javadoc for SearchRequest

--- src/sk/stu/fiit/parsers/Requests/dto/SearchRequest.java
+++ src/sk/stu/fiit/parsers/Requests/dto/SearchRequest.java
@@ -7,6 +7,8 @@ package sk.stu.fiit.parsers.Requests.dto;
 import sk.stu.fiit.parsers.Requests.IRequestVisitor;
 
 /**
+ * SearchRequest request is used to search tour offers whose start place or
+ * destination place contains given expression
  *
  * @author Adam Bublavý
  */
@@ -14,19 +16,39 @@ public class SearchRequest extends Request {
 
     private int pageNumber = 0;
     private int pageSize = 5;
-    
+
     private String destination;
-    
+
+    /**
+     * Creates new SearchRequest
+     *
+     * @param destination Expression which will be searched in tour offers's
+     * start place and destination place
+     */
     public SearchRequest(String destination) {
         this.destination = destination;
     }
 
+    /**
+     * Creates new SearchRequest
+     *
+     * @param query Expression which will be searched in tour offers's start
+     * place and destination place
+     * @param pageNumber Page number
+     * @param pageSize Number of maximum tour offers in page
+     */
     public SearchRequest(String query, int pageNumber, int pageSize) {
         this(query);
         this.pageNumber = pageNumber;
         this.pageSize = pageSize;
     }
-    
+
+    /**
+     * {@inheritDoc}
+     *
+     * {@link IRequestVisitor#constructSearchRequest(sk.stu.fiit.parsers.Requests.dto.SearchRequest)
+     * }
+     */
     @Override
     public void accept(IRequestVisitor visitor) {
         visitor.constructSearchRequest(this);
@@ -43,5 +65,5 @@ public class SearchRequest extends Request {
     public int getPageSize() {
         return pageSize;
     }
-    
+
 }
