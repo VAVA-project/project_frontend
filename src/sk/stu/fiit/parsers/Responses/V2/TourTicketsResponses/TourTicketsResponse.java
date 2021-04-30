37a3620b9f17cff54b093ac875b6fc26daaee89e
Adam Bublavy <bublavy.adam@gmail.com>
2021-04-30 02:15:04 +0200

    Added: Javadoc for TourTicketsResponse, factory and processor

--- src/sk/stu/fiit/parsers/Responses/V2/TourTicketsResponses/TourTicketsResponse.java
+++ src/sk/stu/fiit/parsers/Responses/V2/TourTicketsResponses/TourTicketsResponse.java
@@ -10,19 +10,34 @@ import sk.stu.fiit.Main.TourTicket;
 import sk.stu.fiit.parsers.Responses.V2.Response;
 
 /**
+ * TourTicketsResponse response is used to hold data which are extracted from
+ * tour tickets response
  *
  * @author adamf
+ *
+ * @see TicketsRequest
  */
 public class TourTicketsResponse extends Response {
 
     private List<TourTicket> tourTickets;
     private boolean isLast;
 
+    /**
+     * Creates new TourTicketsResponse
+     *
+     * @param isLast true if response contained last available tour tickets,
+     * false otherwise
+     */
     public TourTicketsResponse(boolean isLast) {
         this.tourTickets = new ArrayList<>();
         this.isLast = isLast;
     }
 
+    /**
+     * Adds tour ticket to list
+     *
+     * @param tourTicket Tour ticket that will be added to list
+     */
     public void addTourTicket(TourTicket tourTicket) {
         this.tourTickets.add(tourTicket);
     }
--- src/sk/stu/fiit/parsers/Responses/V2/TourTicketsResponses/TourTicketsResponseProcessor.java
+++ src/sk/stu/fiit/parsers/Responses/V2/TourTicketsResponses/TourTicketsResponseProcessor.java
@@ -22,55 +22,68 @@ import sk.stu.fiit.parsers.Responses.V2.TourDatesResponses.TourDatesResponseProc
 import sk.stu.fiit.parsers.Responses.V2.XMLProcessor;
 
 /**
+ * TourTicketsResponseProcessor is used to process XML response of fetching
+ * available tickets for specific tour date
  *
  * @author adamf
  */
 public class TourTicketsResponseProcessor extends XMLProcessor {
-    
+
     private static final List<String> possibleValidationErrors
             = Arrays.asList("errors", "sortBy", "sortDirection", "pageNumber",
                     "pageSize");
-    
+
+    /**
+     * {@inheritDoc }
+     */
     @Override
     public List<String> getPossibleValidationErrors() {
         return possibleValidationErrors;
     }
-    
+
+    /**
+     * {@inheritDoc }
+     *
+     * @return Returns parsed data mapped into TourTicketsResponse
+     *
+     * @see TourTicketsResponse
+     */
     @Override
     public Response parseOK(Document document) {
         try {
             XPath xPath = XPathFactory.newInstance().newXPath();
-            
+
             NodeList contentList = (NodeList) xPath.compile(
                     "//PageImpl/content/content").evaluate(document,
                             XPathConstants.NODESET);
-            
+
             String areAllTicketsLoaded = (String) xPath.compile(
                     "//PageImpl/last/text()").
                     evaluate(document, XPathConstants.STRING);
-            
+
             TourTicketsResponse tourTicketsResponse = new TourTicketsResponse(
                     Boolean.valueOf(areAllTicketsLoaded));
-            
+
             for (int i = 0; i < contentList.getLength(); i++) {
-                
+
                 Node node = contentList.item(i);
                 Element element = (Element) node;
-                
+
                 String id = element.getElementsByTagName("id").item(0).
                         getTextContent();
                 String createdAt = element.getElementsByTagName("createdAt").
                         item(0).getTextContent();
                 String updatedAt = element.getElementsByTagName("updatedAt").
                         item(0).getTextContent();
-                
+
                 tourTicketsResponse.addTourTicket(new TourTicket(id, createdAt,
                         updatedAt));
-                
+
             }
-            
-            System.out.println("Parsed " + tourTicketsResponse.getTourTickets().size() + " tickets");
-            
+
+            System.out.println("Parsed " + tourTicketsResponse.getTourTickets().
+                    size() + " tickets");
+
             return tourTicketsResponse;
         } catch (XPathExpressionException ex) {
             Logger.getLogger(TourDatesResponseProcessor.class.getName()).log(
@@ -78,5 +91,5 @@ public class TourTicketsResponseProcessor extends XMLProcessor {
         }
         return null;
     }
-    
+
 }
